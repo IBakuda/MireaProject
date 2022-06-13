698aad8b01265fab2ab21818a29a8481b4cc6ba9
ZoxSpirit <zoxspirit@gmail.com>
2022-06-13 21:17:15 +0300

    first commit

---
+++ app/src/main/java/com/mirea/ivanovma/mireaproject/ui/home/HomeFragment.java
@@ -0,0 +1,56 @@
+package com.mirea.ivanovma.mireaproject.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mirea.ivanovma.mireaproject.LoginActivity;
+import com.mirea.ivanovma.mireaproject.MainActivity;
+import com.mirea.ivanovma.mireaproject.R;
+import com.mirea.ivanovma.mireaproject.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+    private Button loginButton;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loginButton = view.findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/mirea/ivanovma/mireaproject/ui/settings/SettingsFragment.java
@@ -0,0 +1,18 @@
+package com.mirea.ivanovma.mireaproject.ui.settings;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.mirea.ivanovma.mireaproject.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        Toast.makeText(getContext(), "These are your settings", Toast.LENGTH_SHORT).show();
+    }
+}
